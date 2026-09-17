455fc087c90bc91d968f23a66a0ed7cb6a8dc50b
Tural Mehtiyev <54742882+tmehtiyev2019@users.noreply.github.com>
2026-09-17 15:57:50 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,39 @@
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n - 1 == 0:
+#             return nums[0]
+#         elif n - 1 == 1:
+#             return max(nums[0], nums[1])
+#         else:
+#             return max(nums[n-1]+self.rob(nums[0:n-2]),self.rob(nums[0:n-1]))
+
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        
+        
+        memo = {0: nums[0], 1: max(nums[0], nums[1])}
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            res = max(nums[i]+dp(i-2),dp(i-1))
+            memo[i] = res
+            return res
+        return dp(len(nums)-1)
+
+
+#final time: O(n) and space: O(n)
+#edge case: [10,2,3,15,8]
+#for a given nums[i] : max(nums[i-1], nums[i], nums[i+1])
+#calcualte teh max[i]: maximum amount you cna get till ith index
+#recursion with the base case: nums[0], nums[1]--> nums[2]=max(nums[1], nums[0]+nums[2])
+
+
+
+        
