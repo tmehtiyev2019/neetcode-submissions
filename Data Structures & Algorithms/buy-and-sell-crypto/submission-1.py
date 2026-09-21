77211e7a5124aff8e03d1c5a2a2379d9e7fc0980
Tural Mehtiyev <54742882+tmehtiyev2019@users.noreply.github.com>
2026-09-21 12:37:08 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        # min price we have seen so far
+        min_price = 101
+    
+        for i in range(len(prices)):
+            min_price = min(prices[i], min_price)
+            curr_profit = prices[i] - min_price
+            max_profit = max(curr_profit, max_profit)
+
+        return max_profit
+
+# max price we have seen so far
+# min price we have seen so far
+# min price shoudl come before max price
+# what if we also store the index as well
+# or everytim we find a 
