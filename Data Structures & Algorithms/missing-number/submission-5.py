bdcaf7826aa483e6e5edd40eeef3f8723d12339f
Tural Mehtiyev <54742882+tmehtiyev2019@users.noreply.github.com>
2026-09-22 22:22:04 -0400

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.py
@@ -0,0 +1,51 @@
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         for i in range(len(nums)+1):
+#             if i not in nums:
+#                 return i
+
+# Brute Force approach
+# time comp: n^2 n is the length of the nums array
+# space O(1)
+
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         num_set=set(nums) 
+#         for i in range(len(nums)+1):
+#             if i not in num_set:
+#                 return i
+
+
+# # time comp: O(n) n is the length of the nums array
+# # space O(n)
+
+
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         sum_range = 0 # O(1)
+#         for i in range(len(nums)+1): #O(n)
+#             sum_range += i
+#         res = sum_range - sum(nums) #O(n)
+#         return res
+
+
+
+# # time comp: O(n) n is the length of the nums array
+# # space O(1)
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        xorr = n
+        for i in range(len(nums)):
+            xorr = xorr ^ i ^ nums[i]
+        return xorr
+
+
+
+# time comp: O(n) n is the length of the nums array
+# space O(1)
+        
